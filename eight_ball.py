f943fdf9f56a31f470fb2b33eff236d98bc3e6d0
TS878 <46795003+TS878@users.noreply.github.com>
2019-02-04 13:46:17 -0500

    Add files via upload
    An update to the Magic 8-ball
    -Added a title
    -Added the ability to try again

--- eight_ball.py
+++ eight_ball.py
@@ -1,17 +1,47 @@
 ###Imports###
+import os
 from random import randint,choice
 ###Variables###
-yes_answers =['Yes','More than likely','Chances are in your favor']
-no_answers = ['No', 'Not likely', 'Not a chance',
-              'Chances are not in your favor']
+magic = '''
+                      __       __ 
+  /\\  /\\      /\\     /     |  /  \\
+ /  \\/  \\    /__\\    | __  |  |   
+/        \\  /    \\   \\__/  |  \\__/
+                               
+'''
+eight_ball = '''
+  __            
+ /  \           ___
+|    |         |   \      /\    |    |
+ \__/    ___   |___/     /  \   |    |
+ /  \          |   \    /____\  |    |
+|    |         |___/   /      \ |___ |___
+ \__/
+ '''
+answers = {
+'yes':['Yes','More than likely','Chances are in your favor'],
+'no':['No', 'Not likely', 'Not a chance',
+    'Chances are not in your favor', "I wouldn't do that"]
+           }
 
 ###Main Program###
 while True:
-    name = input('\nWhat is your name?:\n')
-    input('Hello %s, what is your question?:\n' % name.title())
-    random = randint(1,2)
-    if random == 1:
-        print(choice(yes_answers))
+    print(magic + eight_ball)
+    name = input('What is your name?:\n')
+    
+    while True:
+        input('Hello %s, what would you like to know?:\n' % name.title())
+        random = randint(1,2)
+        if random == 1:
+            print(choice(answers['yes']))
         
-    elif random == 2:
-        print(choice(no_answers))
+        elif random == 2:
+            print(choice(answers['no']))
+        
+        print('Do you want to ask another question? (yes or no)')
+            
+        a_new_question = input().lower().startswith('y')
+        if a_new_question:
+            os.system('clear')
+        elif not a_new_question:
+            break
